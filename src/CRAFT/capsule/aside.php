ac2cf3dcde67f43a2b8bd93a1869ade20c50133b
manaki-e <48690114s@gmail.com>
2022-04-27 13:56:48 +0900

    #8 sidebarにURL設置（cssはまだ）

--- src/CRAFT/capsule/aside.php
+++ src/CRAFT/capsule/aside.php
@@ -8,7 +8,13 @@
     </li>
     <li class="aside__item">
       <h4 class="aside__title">「CRAFT」サービス</h4>
-      <div class="aside__contents"></div>
+      <ul class="aside__contents aside__content__list">
+        <li class="aside__content__item"><a href="../../CRAFT/index.php">エージェント企業を探す</a></li>
+        <li><a href="../../CRAFT/apply.php">エージェント企業にお問い合わせをする</a></li>
+        <li><a href="../../CRAFT/q&a.php">よくある質問・Q&A</a></li>
+        <li><a href="../../CRAFT/help.php">「CRAFT」へのご要望・ご質問</a></li>
+        <li><a href="../../CRAFT/agent/index.php">エージェント企業担当者の方はこちら</a></li>
+      </ul>
     </li>
     <li class="aside__item">
       <h4 class="aside__title">「CRAFT」とは</h4>
