1ff9cd4f9d613b8302f0c522fe0792cd2d9ade6c
manaki-e <48690114s@gmail.com>
2022-04-29 02:45:50 +0900

    #22 loginPage作成

--- src/CRAFT/agent/login.php
+++ src/CRAFT/agent/login.php
@@ -30,17 +30,44 @@ if (!empty($_POST)) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>管理者ログイン</title>
+  <link rel="stylesheet" href="../../assets/css/reset.css">
+  <link rel="stylesheet" href="../../assets/css/agent_login.css">
 </head>
 
 <body>
-  <div>
-    <h1>管理者ログイン</h1>
-    <form action="/admin/login.php" method="POST">
-      <input type="email" name="email" required>
-      <input type="password" required name="password">
-      <input type="submit" value="ログイン">
-    </form>
-    <a href="">イベント一覧</a>
+  <div class="content">
+    <main class="main">
+      <!-- <h1>管理者ログイン</h1>
+      <form action="/admin/login.php" method="POST">
+        <input type="email" name="email" required>
+        <input type="password" required name="password">
+        <input type="submit" value="ログイン">
+      </form>
+      <a href="">イベント一覧</a> -->
+      <div class="login">
+        <div class="login__inner">
+          <h1 class="login__inner__title"><span>CRAFT</span>エージェント企業画面ログイン</h1>
+          <form action="/" method="" class="login__inner__form">
+            <dl class="login__inner__form__list">
+              <div class="login__inner__form__item">
+                <dt><label for="loginID">ログインID</label></dt>
+                <dd><input id="loginID" type="text" name="loginID"></dd>
+              </div>
+              <div class="login__inner__form__item">
+                <dt><label for="password">パスワード</label></dt>
+                <dd><input id="password" type="text" name="password"></dd>
+              </div>
+              <div class="login__inner__form__footer"></div>
+              <button class="login__inner__form__button" id="submit">ログインする</button>
+            </dl>
+          </form>
+          <p class="login__inner__text">ログインID・パスワードを忘れた方は<a href="#" class="login__inner__nav">こちら</a></p>
+          <hr class="login__inner__border"></hr>
+          <p class="login__inner__text">ログインID・パスワードをお持ちでない方は<a href="#" class="login__inner__nav">こちら</a></p>
+          <button class="login__inner__new">エージェント会員に申し込む</button>
+        </div>
+      </div>
+    </main>
   </div>
 </body>
 
