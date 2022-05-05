517b81ffd0e2097e67a3722e6be62f1631418abd
manaki-e <48690114s@gmail.com>
2022-05-05 16:01:54 +0900

    #7 footer作成

---
+++ src/CRAFT/capsule/footer.php
@@ -0,0 +1,53 @@
+<footer class="footer">
+  <div class="footer__title">
+    <img class="footer__title__img" src="../../assets/img/syukatudotcom_logo_white.png" alt="就活.com">
+    <p class="footer__title__sentence">
+      学生に寄り添い、就職活動をサポートする
+    </p>
+  </div>
+  <ul class="footer__list">
+    <li class="footer__item">
+      <p class="nav__title">SEARCH</p>
+      <ul class="nav__list">
+        <li class="nav__item">特化型</li>
+        <li class="nav__item">総合型</li>
+        <li class="nav__item">特別選考</li>
+        <li class="nav__item">ベンチャー企業</li>
+        <li class="nav__item">大手企業</li>
+      </ul>
+    </li>
+    <li class="footer__item">
+      <p class="nav__title">CRAFT</p>
+      <ul class="nav__list">
+        <li class="nav__item">「CRAFT」とは</li>
+        <li class="nav__item">エージェント企業を探す</li>
+        <li class="nav__item">エージェント企業にお問い合わせ</li>
+        <li class="nav__item">よくある質問</li>
+        <li class="nav__item">ご利用方法</li>
+        <li class="nav__item">エージェント企業の方へ</li>
+      </ul>
+    </li>
+    <li class="footer__item">
+      <p class="nav__title">OTHER SERVICE</p>
+      <ul class="nav__list">
+        <li class="nav__item">サービス１</li>
+        <li class="nav__item">サービス２</li>
+        <li class="nav__item">サービス３</li>
+        <li class="nav__item">サービス４</li>
+      </ul>
+    </li>
+    <li class="footer__item">
+      <p class="nav__title">COMPANY</p>
+      <ul class="nav__list">
+        <li class="nav__item">会社概要</li>
+        <li class="nav__item">求人情報</li>
+        <li class="nav__item">プライバシーポリシー</li>
+        <li class="nav__item">利用規約</li>
+        <li class="nav__item">お問い合わせ</li>
+      </ul>
+    </li>
+  </ul>
+  <div class="footer__copyright">
+    <small>Copyright © boozer.Inc</small>
+  </div>
+</footer>
