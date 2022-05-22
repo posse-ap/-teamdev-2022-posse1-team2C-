ec0856dde81372fee99ae26b9a5d3c0b7253dc83
manaki-e <48690114s@gmail.com>
2022-05-22 17:06:02 +0900

    #21 見た目のみ完成（動きなし）

---
+++ src/assets/js/diagnosis.js
@@ -0,0 +1,9 @@
+let theme1 = 現在の状況;
+let theme2 = 思考スタイル;
+let theme3 = 就活に対する価値観;
+
+
+let question1_1 = "卒業年度";
+let question1_2 = "居住地";
+let question1_3 = "内定をすでに獲得している";
+let question1_4 = "インターンシップをしている。または、過去にしたことがある";
